f9121ebeb13987647868a3a4716a77927719ddb9
stayFoolish <nemanjakaraklajic90@icloud.com>
2025-01-26 12:09:08 +0100

    Implement Suspense for event data loading with React Router
    Refactor event loading to use Suspense and Await for better user experience. Adjust loaders to handle asynchronous data fetching and add a fallback loading message. Introduce a delay in backend events response for testing purposes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home.tsx";
-import Events from "./pages/Events.tsx";
+import Events, { loader as eventsLoader } from "./pages/Events.tsx";
 import EventDetail, { deleteItemAction } from "./pages/EventDetail.tsx";
 import NewEvent from "./pages/NewEvent.tsx";
 import EditEvent from "./pages/EditEvent.tsx";
 import RootLayout from "./layouts/RootLayout.tsx";
 import EventsRoot from "./layouts/EventsRoot.tsx";
-import { eventsLoader } from "./pages/Events.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import SelectedEvent, { eventItemLoader } from "./layouts/SelectedEvent.tsx";
 import { action as manipulateEventsAction } from "./components/EventForm.tsx";
--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
 import EventsList from "../components/EventsList.tsx";
 import { Event } from "../types";
-import { useLoaderData } from "react-router";
+import { Await, useLoaderData } from "react-router";
 import { API_URL } from "../config.ts";
 
 const Events: React.FC = () => {
@@ -9,10 +9,19 @@ const Events: React.FC = () => {
    * We can use useLoaderData() in the element that's assigned to a route AND in all components that might be used inside that element(children).
    */
 
-  const loaderData: { events: Event[] } = useLoaderData();
-  const { events } = loaderData;
+  const { events }: { events: Event[] } = useLoaderData();
 
-  return <EventsList events={events} />;
+  console.log({ events });
+
+  return (
+    <Suspense
+      fallback={<div style={{ textAlign: "center" }}>Loading Events...</div>}
+    >
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 };
 
 export default Events;
@@ -23,7 +32,8 @@ export default Events;
  * Data fetching is initiated as soon as route got initiated. In this case loader function is called after we go to the route: "/events".
  * React router will wait for data to be fetched (loader to be finished) before it renders the page with that data.
  */
-export async function eventsLoader() {
+
+export async function loadEvents() {
   const response = await fetch(`${API_URL}/events`);
 
   if (!response.ok) {
@@ -35,6 +45,13 @@ export async function eventsLoader() {
       status: 500,
     });
   }
+  const responseData = await response.json();
 
-  return response;
+  return responseData.events;
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
